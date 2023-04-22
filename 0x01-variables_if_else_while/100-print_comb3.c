6f28d27c6eb45d849138cd7caadb3dea676349e5
Asare Angela Konadu <asareangelakonadu@gmail.com>
2023-04-22 14:56:06 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int c;
+int d = 0;
+
+while (d < 10)
+{
+c = 0;
+while
+(c < 10)
+{
+if (d != c && d < c)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
