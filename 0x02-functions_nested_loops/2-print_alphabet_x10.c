37d279651aa0f0e95cb9725f80dfa1f3db58ec9c
Asare Angela Konadu <asareangelakonadu@gmail.com>
2023-04-26 18:58:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 time
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
 
@@ -9,15 +9,19 @@ void print_alphabet_x10(void)
 
 {
 
-	char c;
-	int i = 0;
+char c;
+int i;
+i = 0;
+while (i < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-			_putchar('\n');
-			i++;
-		}
-	}
+
+}
+	
